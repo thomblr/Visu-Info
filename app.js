059cd3be3d90383e04d7d4e1d3a15da62750d4cd
thomblr <thomas.1111@live.be>
2020-10-29 18:33:58 +0100

    Load all data

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
-fetch("datasets/fossils.json")
-    .then(function(resp) {
-        return resp.json();
-    })
-    .then(function(data) {
-        console.log(data);
-    })
+d3.json("datasets/fossils.json").then(function(data) {
+    console.log(data[0]);
+});
+
+d3.json("datasets/gts_tree.json").then(function(data) {
+    console.log(data[0]);
+});
+
+d3.csv("datasets/dinosaurs.csv").then(function(data) {
+    console.log(data[0]);
+});
+
+d3.tsv("datasets/gts.csv").then(function(data) {
+    console.log(data);
+
+    data.forEach((item, index) => console.log(index, item['type']))
+
+});
