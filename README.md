a4c423da6e7024a8c8a7941c40f335d964110c36
thomblr <thomas.1111@live.be>
2020-12-05 12:03:57 +0100

    Modifications

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # Visu-Info
 Project of Information Visualization based on a dataset about fossils.
+
+## Dataset
+
+### dinosaurs.csv
+
+Features of 29 dinosaurs.
+
+- id of the dinosaur
+- 'diet' : Diet of the dinosaur (Carnivorous, Herbivorous, Omnivorous)
+- 'dinosaur' : Name of the dinosaur
+- 'lived' : Time during which the dinosaur roamed the Earth, might be specified as an interval
+- 'size' : Size of the dinosaur in meters
+- 'speed' : Maximum speed of the dinosaur, either in kmh or in mph
+- 'weight' : Weight of the dinosaur, in kilograms 
+
+### fossils.json
+
+### gts_tree.json
+
+### gts.tsv
--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 mapboxgl.accessToken = 'pk.eyJ1IjoidGhvbWJsciIsImEiOiJja2dxbmdseTkwNnVpMnlxd2RjcXF2bHd2In0.-nQuV-bVYgtHWJls-534pw';
 
-var map = new mapboxgl.Map({
+const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/outdoors-v11', // stylesheet location
     center: [4.3528, 50.8466], // starting position [lng, lat]
@@ -9,9 +9,53 @@ var map = new mapboxgl.Map({
     maxZoom: 9
 });
 
+map.addControl(new mapboxgl.NavigationControl({
+        position: "top-left" // The controls appear at the top left
+    }
+));
+
+const features = Array();
+
 d3.json("datasets/fossils.json").then(function(data) {
-    // data.forEach((item, index) => new mapboxgl.Marker().setLngLat([item['longitude'], item['latitude']]).addTo(map));
+    for(let i = 0; i < data.length; i++) {
+        let point = {
+            type : "Feature",
+            geometry : {
+                type : "Point",
+                coordinates : [data[i].longitude, data[i].latitude],
+            },
+            properties : {
+                name : data[i].name,
+            }
+        }
+        features.push(point);
+    }
+
+    //data.forEach((item, index) => new mapboxgl.Marker().setLngLat([item['longitude'], item['latitude']]).addTo(map));
 });
+
+map.on("load", function() {
+    let featureCollection = {type : "FeatureCollection", features : features};
+
+    map.addSource('dinos', {
+        'type': 'geojson',
+        'data': featureCollection
+    });
+
+    map.addLayer({
+        id: "dinosaurs",
+        type: "circle",
+        source: 'dinos',
+        paint: {
+            "circle-radius": 5,
+            "circle-color": "#0082a3",
+            "circle-opacity": 0.6,
+            "circle-stroke-width": 1,
+            "circle-stroke-color": "#004d60"
+        }
+    });
+});
+
 
 d3.json("datasets/gts_tree.json").then(function(data) {
     // console.log(data[0]);
